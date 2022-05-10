32bac42d402f93c63a1382c5a78f0932a188e3d5
yulisuliman <yulisuliman1234@gmail.com>
2022-05-10 19:38:22 +0300

    Adding user's posts to their profile

--- users/views.py
+++ users/views.py
@@ -4,11 +4,16 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import DetailView
 from users.models import Profile
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from posts.models import Post
 
 class ProfileDetailView(LoginRequiredMixin, DetailView):
     model = Profile
     template_name = 'users/profile_details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts_list'] = Post.objects.filter(author=self.get_object().user)
+        return context
 
     def get_slug_field(self):
         """Get the name of a slug field to be used to look up by slug."""
